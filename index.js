1acf9c9e3c5ae1213819310c43f30375f5bb42c3
Kanae Lee <83003828+kanaenumatalee@users.noreply.github.com>
2022-12-31 17:22:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
 let teamnum1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
-let score = 0;
+let score1 = 0;
+let score2 = 0;
 
 function addOne(team) {
-    score+=1;
-    team.textContent = score;
+    if(team === num1) {
+        score1+=1;   
+        team.textContent = score1;    
+    }
+    if(team === num2) {
+        score2+=1;
+        team.textContent = score2; 
+    }
 }
 
 function addTwo(team) {
-    score+=2;
-    team.textContent = score;
+    if(team === num1) {
+        score1+=2;   
+        team.textContent = score1;    
+    }
+    if(team === num2) {
+        score2+=2;
+        team.textContent = score2; 
+    }
 }
 
 function addThree(team) {
-    score+=3;
-    team.textContent = score;
+    if(team === num1) {
+        score1+=3;   
+        team.textContent = score1;    
+    }
+    if(team === num2) {
+        score2+=3;
+        team.textContent = score2; 
+    }
 }
 
 function reset() {
-    score = 0;
-    num1.textContent = score;
-    num2.textContent = score;
+    score1 = 0;
+    score2 = 0;
+    num1.textContent = score1;
+    num2.textContent = score2;
 }
